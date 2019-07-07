eff785cfdc8ac8ca9661a46fd2ad87771a8b9201
sebastian incarbone <sebastianincarbonew@gmail.com>
2019-07-06 21:05:46 -0300

    home fix

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,45 @@
 <template>
   <div id="app">
+
     <background id="bg"/>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <home id="bienvenida"/>
+  
+    <!--
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+        <router-view/> 
+    -->
+  
   </div>
 </template>
 
 <script>
-import background from '@/components/background.vue'
+import background from '@/components/Background.vue'
+import home from '@/components/Home.vue'
 
 export default {
-  components:{ background }  
+  components:{ 
+    background,
+    home
+  }  
 }
 </script>
 
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #bg{
+    position: absolute;
+    top:0px;
+    left:0px;
+    max-width: 90vw;
+    z-index: -1;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-#bg{
-  position: absolute;
-  top:0px;
-  left:0px;
-  max-width: 100vw;
-  z-index: -1;
-}
+  #bienvenida{
+    margin-top: 12vh;
+  }
+  
 </style>
