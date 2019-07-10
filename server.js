e2929b533cb0dc3de5018e459d707ebcb27cebcb
sebastian incarbone <sebastianincarbonew@gmail.com>
2019-07-10 18:20:54 -0300

    fix express

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const port = process.env.PORT || 8080;
 
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-const port = process.env.PORT || 5000;
+app.get(/./*,function(req, res){
+  res.sendfile(__dirname + "/dist/index.html");
+});
+
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
